1aa0827fe5c7b282b8b307194a93a22bb28d0fef
t-kuha <imagingtechnerd@gmail.com>
2020-04-24 16:06:03 +0900

    Update create_pfm.tcl

--- accel/create_pfm.tcl
+++ accel/create_pfm.tcl
@@ -1,10 +1,17 @@
+#
 # Create acceleration platform
+#
 
-platform create -name {sd_blk_accel} -hw {sd_blk_accel.xsa} -no-boot-bsp -out {_pfm}
+set PFM_NAME    sd_blk_accel
+set OUT_DIR     _pfm
+
+# Remove existing directory
+file delete -force ${OUT_DIR}
+
+platform create -name ${PFM_NAME} -hw ${PFM_NAME}.xsa -no-boot-bsp -out ${OUT_DIR}
 platform write
-platform active {sd_blk_accel}
+platform active ${PFM_NAME}
 
-# Add sysroot??
 domain create -name xrt -os linux -proc ps7_cortexa9
 domain active xrt
 domain config -bif {src/linux.bif}
