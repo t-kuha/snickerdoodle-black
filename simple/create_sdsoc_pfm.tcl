9db1e327ed33eae56c63b639031db9537a48717d
t-kuha <imagingtechnerd@gmail.com>
2020-09-13 00:34:50 +0000

    Remove create_sdsoc_pfm.tcl

--- simple/create_sdsoc_pfm.tcl
+++ simple/create_sdsoc_pfm.tcl
@@ -3,7 +3,10 @@
 # Platform name
 set PFM_NAME    sd_blk
 set SRC_DIR     pfm_files
-set PREBUILT_DATA_DIR   ${SRC_DIR}/prebuilt
+set PREBUILT_DATA_DIR   ${SRC_DIR}/_prebuilt
+set BOOT_DIR    ${SRC_DIR}/_boot
+set IMG_DIR     ${SRC_DIR}/_image
+set PLNX_ROOT   petalinux
 
 # Check whether prebuilt data folder exists
 set HAS_PREBUILT [file exists ${PREBUILT_DATA_DIR}]
@@ -14,16 +17,27 @@ if { ${HAS_PREBUILT} } then {
     set OUTDIR  _pfm_0
 }
 
+# Remove existing directory
+file delete -force ${OUTDIR}
+file delete -force ${BOOT_DIR}
+file delete -force ${IMG_DIR}
+
+file mkdir ${BOOT_DIR}
+file mkdir ${IMG_DIR}
+
+# Copy necessary output products
+file copy ${PLNX_ROOT}/images/linux/u-boot.elf      ${BOOT_DIR}
+file copy ${PLNX_ROOT}/images/linux/zynq_fsbl.elf   ${BOOT_DIR}
+file copy ${PLNX_ROOT}/images/linux/image.ub        ${IMG_DIR}
 
 # Create platform
 platform create -name ${PFM_NAME} -hw ${PFM_NAME}.dsa -out ${OUTDIR}
-# {platform}
 
 
 # ----- Standalone -----
 sysconfig create -name "standalone" -display-name "standalone" -desc "Standalone / Zynq-7000" 
 
-sysconfig config -boot {pfm_files/boot}
+sysconfig config -boot ${BOOT_DIR}
 sysconfig config -bif {pfm_files/sdsoc_standalone.bif}
 sysconfig config -readme {pfm_files/generic.readme}
 
@@ -45,7 +59,7 @@ platform write
 # Create system configuration
 sysconfig create -name "linux" -display-name "linux" -desc "Linux / Zynq-7000" 
 
-sysconfig config -boot {pfm_files/boot}
+sysconfig config -boot ${BOOT_DIR}
 sysconfig config -bif {pfm_files/sdsoc_linux.bif}
 sysconfig config -readme {pfm_files/generic.readme}
 
@@ -58,7 +72,7 @@ if { ${HAS_PREBUILT} } then {
 
 ::scw::set_linux_configured true
 
-domain config -image {pfm_files/image}
+domain config -image ${IMG_DIR}
 platform write
 
 
