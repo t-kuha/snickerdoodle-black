ec8da760539b5585ff43b5a9c429509d345789ea
t-kuha <imagingtechnerd@gmail.com>
2020-09-13 12:46:59 +0000

    Update create_sdsoc_pfm.tcl

--- simple/create_sdsoc_pfm.tcl
+++ simple/create_sdsoc_pfm.tcl
@@ -1,18 +1,32 @@
 # May have to delete existing platform directory
 
 # Platform name
-set PFM_NAME    sd_blk
-set SRC_DIR     pfm_files
+set PFM_NAME            sd_blk
+set SRC_DIR             pfm_files
 set PREBUILT_DATA_DIR   ${SRC_DIR}/_prebuilt
-set BOOT_DIR    ${SRC_DIR}/_boot
-set IMG_DIR     ${SRC_DIR}/_image
-set PLNX_ROOT   petalinux
+set BOOT_DIR            ${SRC_DIR}/_boot
+set IMG_DIR             ${SRC_DIR}/_image
+set PREBUILT_DIR        ${SRC_DIR}/_prebuilt
+set INIT_PRJ_DIR        _prj_0
+set PLNX_ROOT           petalinux
 
 # Check whether prebuilt data folder exists
-set HAS_PREBUILT [file exists ${PREBUILT_DATA_DIR}]
+set HAS_PREBUILT [file exists ${INIT_PRJ_DIR}]
 
 if { ${HAS_PREBUILT} } then {
     set OUTDIR  _pfm
+    file delete -force ${PREBUILT_DIR}
+    file mkdir ${PREBUILT_DIR}
+
+    # system.bit file should be renamed to bitstream.bit
+    file copy ${INIT_PRJ_DIR}/_sds/p0/vpl/system.bit    ${PREBUILT_DIR}/bitstream.bit
+
+    # system.hdf file should be renamed to <platform>.hdf
+    file copy ${INIT_PRJ_DIR}/_sds/p0/vpl/system.hdf    ${PREBUILT_DIR}/sd_blk.hdf
+    file copy ${INIT_PRJ_DIR}/_sds/.llvm/partitions.xml ${PREBUILT_DIR}
+    file copy ${INIT_PRJ_DIR}/_sds/.llvm/apsys_0.xml    ${PREBUILT_DIR}
+    file copy ${INIT_PRJ_DIR}/_sds/swstubs/portinfo.c   ${PREBUILT_DIR}
+    file copy ${INIT_PRJ_DIR}/_sds/swstubs/portinfo.h   ${PREBUILT_DIR}
 } else {
     set OUTDIR  _pfm_0
 }
