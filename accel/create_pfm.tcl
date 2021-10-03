21dd2c3cc223518e3cb86906976c311359baf65a
t-kuha <imagingtechnerd@gmail.com>
2021-10-03 10:57:18 +0900

    Add source files for creating platform

---
+++ accel/create_pfm.tcl
@@ -0,0 +1,32 @@
+#
+# Create Vitis Acceleration Platform
+#
+
+set PFM_NAME    sd_blk_accel
+set OUT_DIR     _pfm
+set IMG_DIR     _img
+
+# Remove existing directories
+file delete -force ${OUT_DIR}
+file delete -force ${IMG_DIR}
+
+# Copy necessary files
+file mkdir ${IMG_DIR}
+file copy petalinux/images/linux/boot.scr ${IMG_DIR}
+file copy petalinux/images/linux/image.ub ${IMG_DIR}
+
+# Generate
+platform create -name ${PFM_NAME} -hw {sd_blk.xsa} -no-boot-bsp -out ${OUT_DIR}
+platform write
+platform active ${PFM_NAME}
+
+domain create -name xrt -os linux -proc ps7_cortexa9
+domain active xrt
+# domain config -generate-bif
+domain config -bif {src/linux.bif}
+domain config -boot {petalinux/images/linux}
+domain config -sd-dir ${IMG_DIR}
+domain config -qemu-data {petalinux/images/linux}
+platform write
+
+platform generate
